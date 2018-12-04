8771aad5b65a63172a3c215fa19d9f2b6839e48c
kwikler95 <44887468+kwikler95@users.noreply.github.com>
2018-12-03 16:48:29 -0800

    Add files via upload

--- lab5_eqns.m
+++ lab5_eqns.m
@@ -5,33 +5,22 @@ function [ds, ext] = lab5_eqns(t,s);
 global Rw Lw Tm M bt R Gr Cr g Cd rho Af n vi uinss Fb im p_in Kp Ki
 
 % renaming variables for clarity
-p_3 = s(1); %momentum to inductor
-p_9 = s(2); %momentum to car mass
+p_3 = s(1); 
+p_9 = s(2); 
 d_ref = s(3);
 d = s(4);
 
 % define u_in and Fb time
-T1=0.5;
-T2=2;
+T1 = 0.1;
 
-%defining uin and Fb and v
-if t <= T1;
-	u_in = uinss; %initial voltage
-    fb=0;
+if t >= T1;
+    %vref = 1;
+    vref = LA92Oracle(t);
+else
     vref = 0;
-elseif t >= T1 && t <= T2;
-	%u_in = uinss.*(1-(2/3).*(t-0.5)); %ramp down
-    u_in = Kp*(1-(p_9/M)) + Ki*(d_ref - d);
-    fb=Fb;
-    vref = 1;
-else  t > T2;
-	u_in = 0; %zero voltage
-    fb=0; % zero voltage
-    vref = 1; 
 end
 
-
-% friction brake input
+u_in = Kp*(vref-(p_9/M)) + Ki*(d_ref - d);
 
 % non linear efforts
 v=p_9/M; %car velocity
--- lab5_master.m
+++ lab5_master.m
@@ -21,27 +21,20 @@ rho = 1.21; %kg/m^3, air density
 Af = 2.05; %m^2 vehicle frontal area
 n = 0.0001; 
 vi = 20; %m/s initial vehicle speed
-im = 0; %initial currrent in recharging circuit
+im = 0; %i200nitial currrent in recharging circuit
 k = (Tm^2)/Lw; %stiffness, simplification of I/GY to C
-Kp = 200; %proportional gain
-Ki = 30; %integral gain
-vref=1;
+Kp = 165; %proportional gain
+Ki = 500; %integral gain
 
-%p_3in = Lw*im; %initial inductor momentum
-e11_in = 0.5*rho*Af*Cd*20*20;
-p_3in = (e11_in*R/Gr + bt*(Gr/R)*(20))*(Lw/Tm);
-p_9in = M*vi; %initial car momentum
-uinss = im*Rw+Tm*(Gr/R)*20;
+
 
 %% Run ODE45 Solver 
 
 % define initial conditions
-%initial = [p_3in p_9in]; 
 initial = [0 0 0 0];
 
 % setup time array
-%tspan = [0:.025:3]; %defined in lab handout
-tspan = [0:0.01:2];
+tspan = [0:0.05:300];
 
 % call ODE solver
 [t, s] = ode45(@lab5_eqns,tspan,initial);
@@ -55,32 +48,49 @@ for i = 1:length(t)
     [ds(i,:) ext(i,:)] = lab5_eqns(t(i), s(i,:));
 end
 
-%% current and nat frequency
+vref = zeros(length(t),1);
+for i = 1:length(t)
+    vref(i) = LA92Oracle(t(i));
+end
 
-current=ext(:,1)/Lw;
+%% Bode plot
+
+A = [(-Rw/Lw) -(Tm*Gr)/(R*M); (Tm*Gr)/(R*Lw) -(bt*Gr^2)/(M*R^2)];
+B = [1;0];
+C = [0;1/M];
+D = [0];
+
+s = tf('s');
+I = eye(2);
+
+p = (s*I - A)^-1;
+
+TF = C*p*B + D;
+figure
+bode(TF)
+
+
 
 %% Plot Figures
 
-figure('Name','velocity','NumberTitle','off','Color','white')
-plot(t,s(:,2)/M,'k'), grid on
-title('Vehicle Speed vs Time')
-legend('Vehicle Speed')
+figure('Name','Actual and Reference Velocity Profiles','NumberTitle','off','Color','white')
+plot(t,ds(:,4),'r',t,ds(:,3),'--k'), grid on
+title('Actual and Reference Velocity Profiles')
+legend('Actual Vehicle Velocity','LA92 Reference Velocity','Location','Best')
+axis([32,54,4,8])
+ylabel('Response (m/s)')
+xlabel('Time (s)')
+
+figure('Name','Actual Velocity Profile','NumberTitle','off','Color','white')
+plot(t,ds(:,4),'k'), grid on
+title('Actual Velocity Profile')
+legend('Actual Velocity')
 ylabel('Speed (m/s)')
 xlabel('Time (s)')
-
-figure('Name','current','NumberTitle','off','Color','white')
-plot(t,current,'k'), grid on
-title('Current vs Time')
-legend('Current')
-ylabel('Current (Amps)')
-xlabel('Time (s)')
- 
-figure('Name','Step Response','NumberTitle','off','Color','white')
-plot(t,ds(:,3),'--k',t,-s(:,2)/M,'m'), grid on
-title('Step Response')
-legend('Step Input','Output Response')
-ylabel('Response')
-xlabel('Time (s)')
-
-min(current)
-wn = sqrt(k/Lw)
+% 
+% figure('Name','current','NumberTitle','off','Color','white')
+% plot(t,current,'k'), grid on
+% title('Current vs Time')
+% legend('Current')
+% ylabel('Current (Amps)')
+% xlabel('Time (s)')
